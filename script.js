40c7b9824fc854c75a567c923e776a921bcda961
shadi.jahanpanah <Sh64.Fa77_1381@yahoo.com>
2025-08-26 13:57:05 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+ document.addEventListener("DOMContentLoaded", function () {
   const cake = document.querySelector(".cake");
   const candleCountDisplay = document.getElementById("candleCount");
   let candles = [];
